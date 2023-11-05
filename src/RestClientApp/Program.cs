2221ccad62e4bcc9ab57afa32ab1e590f1018905
diamantis <diant@live.com>
2023-11-05 09:19:44 +0100

    .

---
+++ src/RestClientApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+Console.WriteLine("Hello, World!");
+string port;
+if (args is null || args.Length == 0)
+{
+    Console.Write("Port? ");
+    port = Console.ReadLine()!;
+}
+else
+{
+    port = args![0];
+}
+var url = $"http://localhost:{port}";
+var httpClient = new HttpClient
+{
+    BaseAddress = new Uri(url)
+};
+httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+while (true)
+{
+    Console.Write("Action? ");
+    var action = Console.ReadLine();
+
+    Console.WriteLine($"Calling {url}/{action} ...");
+    var response = await httpClient.GetAsync($"/{action}");
+    var content = await response.Content.ReadAsStringAsync();
+    var obj = JsonConvert.DeserializeObject<Result>(content);
+    Console.WriteLine(content);
+}
+
+record Result(string Message);
